04e6870bb273a2d3d8ee84fabfcb4ec7f0cb1edb
Tran Thi Thu Hang <ttthang0102gmail.com>
2020-10-07 14:39:03 +0700

    hinh chu nhat

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+include ("Thuc_hanh.php");
+?>
+
+<?php
+
+$rec1 = new Rectangle(10,20);
+
+echo $rec1->width;//10
+echo $rec1->height;//20
+
+$rec1->width=30;
+$rec1->height=20;
+
+echo $rec1->width;//30
+echo $rec1->height;//20
+echo $rec1->getArea();//600
+echo $rec1->getPerimeter();//100
+echo "your rectangle is ".$rec1->display();
+?>
